7a4bda1dcfab37e118b2681eedfdcb4d1c9333af
Hudo <hrvoje.hudoletnjak@hmhco.com>
2016-03-03 12:43:52 +0000

    Initial implementation with few tests

---
+++ PatternMatch.Tests/PatternTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace PatternMatch.Tests
+{
+    public class PatternTests
+    {
+        [TestCase(150, "> 100")]
+        [TestCase(70, "> 50")]
+        [TestCase(20, "> 10")]
+        [TestCase(2, "")]
+        public void Condition_with_action(int n, string expected)
+        {
+            var result = Pattern.Match<int, string>(n)
+                .When(x => x > 100, () => "> 100")
+                .When(x => x > 50, () => "> 50")
+                .When(x => x > 10, () => "> 10")
+                .Default(() => "");
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [Test]
+        public void Match_type()
+        {
+            var result = Pattern.Match<IBase, int>(new Foo() {A = 5})
+                .When<IFoo>(foo => foo.A)
+                .When<IBoo>(boo => boo.B)
+                .Default(() => 0);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Instance_value()
+        {
+            var value = 5;
+            var result = Pattern.Match<object, string>(value)
+                .When(5, () => value.ToString())
+                .When("10", () => "10")
+                .When(new Foo(), () => "foo")
+                .Result;
+
+            Assert.AreEqual("5", result);
+        }
+
+        interface IFoo : IBase { int A { get; } }
+        class Foo : IFoo { public int A { get; set; } }
+
+        interface IBoo : IBase { int B { get; } }
+        class Boo : IBoo { public int B { get; set; } }
+
+        interface IBase { }
+    }
+}
---
+++ PatternMatch/Pattern.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace PatternMatch
+{
+    /// <summary>
+    /// Poor mans pattern matching. 
+    /// Use it instead of switch/case, for more functional approach
+    /// </summary>
+    public static class Pattern
+    {
+        public static PatternMatch<T, R> Match<T, R>(T value)
+        {
+            return new PatternMatch<T, R>(value);
+        }
+
+        public static PatternMatch<T> Match<T>(T value)
+        {
+            return new PatternMatch<T>(value);
+        }
+
+        public struct PatternMatch<T>
+        {
+            private PatternMatch<T, Nothing> _inner;
+
+            public PatternMatch(T value)
+            {
+                _inner = new PatternMatch<T, Nothing>(value);
+            }
+
+            public PatternMatch<T> When(Func<T, bool> condition, Action action)
+            {
+                _inner.When(condition, () => { action(); return Nothing.Instance; });
+
+                return this;
+            }
+
+            public PatternMatch<T> When<C>(Action<C> action)
+            {
+                _inner.When<C>(c => { action(c); return Nothing.Instance; });
+                return this;
+            }
+
+            public PatternMatch<T> When<C>(Func<C, bool> condition, Action<C> action)
+            {
+                _inner.When<C>(condition, c => { action(c); return Nothing.Instance; });
+                return this;
+            }
+
+            public PatternMatch<T> When<C>(C value, Action action)
+            {
+                _inner.When<C>(value, () => { action(); return Nothing.Instance; });
+                return this;
+            }
+
+            public void Default(Action action)
+            {
+                _inner.Default(() => { action(); return Nothing.Instance; });
+            }
+        }
+
+        public struct PatternMatch<T, R>
+        {
+            private readonly T _value;
+            private R _result;
+
+            private bool _matched;
+
+            public PatternMatch(T value)
+            {
+                _value = value;
+                _matched = false;
+                _result = default(R);
+            }
+
+            public PatternMatch<T, R> When(Func<T, bool> condition, Func<R> action)
+            {
+                if (!_matched && condition(_value))
+                {
+                    _result = action();
+                    _matched = true;
+                }
+
+                return this;
+            }
+
+            public PatternMatch<T, R> When<C>(Func<C, R> action)
+            {
+                if (!_matched && _value is C)
+                {
+                    _result = action((C)(object)_value);
+                    _matched = true;
+                }
+                return this;
+            }
+
+            public PatternMatch<T, R> When<C>(Func<C, bool> condition, Func<C, R> action)
+            {
+                if (!_matched && _value is C && condition((C)(object)_value))
+                {
+                    _result = action((C)(object)_value);
+                    _matched = true;
+                }
+                return this;
+            }
+
+
+            public PatternMatch<T, R> When<C>(C value, Func<R> action)
+            {
+                if (!_matched && value.Equals(_value))
+                {
+                    _result = action();
+                    _matched = true;
+                }
+                return this;
+            }
+
+
+            public R Result => _result;
+
+            public R Default(Func<R> action)
+            {
+                return !_matched ? action() : _result;
+            }
+        }
+
+        /// <summary>
+        /// This class can be used instead of void for return types.
+        /// </summary>
+        private class Nothing
+        {
+            public static readonly Nothing Instance = new Nothing();
+
+            private Nothing() { }
+
+            public override string ToString()
+            {
+                return "Nothing";
+            }
+        }
+    }
+}
